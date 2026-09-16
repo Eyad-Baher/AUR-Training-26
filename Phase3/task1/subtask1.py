7bd18c579ec085f4f147dfd80c15b335902e50c1
Eyad-Baher <eaydbaher69@gmail.com>
2026-09-16 19:50:14 +0300

    task 1 updated

--- Phase3/task1/subtask1.py
+++ Phase3/task1/subtask1.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-img = cv2.imread(r'sesesion1\task1\face.jpg')
+img = cv2.imread(r'Phase3\task1\face.jpg')
 
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
